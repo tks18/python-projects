d5bd495e487fc91168d062402aa9ff883ee303d3
tks18 <me@shaaan.tk>
2021-03-10 17:10:30 +0530

    Update Pomodoro Timer

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -1,5 +1,7 @@
 
 # ---------------------------- CONSTANTS ------------------------------- #
+from tkinter import *
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -9,10 +11,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomatoImage = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomatoImage)
+canvas.create_text(100, 112, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+window.mainloop()
